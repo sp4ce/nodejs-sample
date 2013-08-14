6380074042f795c591ade696b121fcce8a1b76f5
Baptiste <bapt@sp4ce.net>
2013-08-14 09:17:47 -0500

    sqlite3 test

--- web.js
+++ web.js
@@ -2,8 +2,30 @@ var express = require("express");
 var app = express();
 app.use(express.logger());
 
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(':memory:');
+
+db.serialize(function() {
+	db.run("CREATE TABLE lorem (info TEXT)");
+
+	var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+	for (var i = 0; i < 10; i++) {
+		stmt.run("Ipsum " + i);
+	}
+	stmt.finalize();
+});
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+	console.log('coucou');
+	db.serialize(function() {
+	  var msg = '';
+		db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+      //console.log(row.id + ": " + row.info);
+			msg += row.id + ": " + row.info + '<br />';
+		}, function(err, num) {
+			response.send(msg);
+		});
+	});
 });
 
 var port = process.env.PORT || 5000;
