639f422aae1f12330b591e8dbad0c9e54c218915
Baptiste <bapt@sp4ce.net>
2013-08-16 08:28:05 -0500

    Account registration

--- public/angular/app.js
+++ public/angular/app.js
@@ -1,18 +1,33 @@
-angular.module('app', [])
-	.directive('usernameunique', function() {
-		return {
-			require: 'ngModel',
-			link: function(scope, elm, attrs, ctrl) {
-				// view -> model
-				elm.on('blur', function() {
-					ctrl.$setValidity('checking', false);
-					scope.$apply(function() {
-						scope.is_unique(elm.val(), function(unique) {
-							ctrl.$setValidity('checking', true);
-							ctrl.$setValidity('used', unique);
-						});
+angular.module('app', ['ngRoute'], function($routeProvider){
+	$routeProvider.when('/', {
+    templateUrl: 'angular/templates/signup.html',
+    controller: RegisterController
+  });
+
+	$routeProvider.when('/register/success', {
+    templateUrl: 'angular/templates/signup-success.html',
+    controller: RegisterController
+  });
+}).directive('usernameunique', function() {
+	return {
+		require: 'ngModel',
+		link: function(scope, elm, attrs, ctrl) {
+			// view -> model
+			elm.on('blur', function() {
+				ctrl.$setValidity('checking', false);
+				scope.$apply(function() {
+					scope.is_unique(elm.val(), function(unique) {
+						ctrl.$setValidity('checking', true);
+						ctrl.$setValidity('used', unique);
 					});
 				});
-			}
-		};
-	});
+			});
+		}
+	};
+}).directive('focus', function () {
+	return function (scope, element, attrs) {
+		attrs.$observe('focus', function (newValue) {
+			newValue === 'true' && element[0].focus();
+		});
+	}
+});
--- public/angular/controller.js
+++ public/angular/controller.js
@@ -1,4 +1,4 @@
-function RegisterController($scope, $http) {
+function RegisterController($scope, $rootScope, $http) {
 
 	$scope.is_unique = function(value, callback) {
 		$http.get('/users').success(function(users) {
@@ -11,10 +11,16 @@ function RegisterController($scope, $http) {
 			name: $scope.username,
 			password: $scope.password }
 		).success(function(user_id) {
-			console.log(user_id);
+			window.location.hash = "register/success";
+			$rootScope.$broadcast('registerSuccessEvent', $scope.username);
 		});
 	}
 }
 
 function LoginController($scope) {
+
+	$scope.$on('registerSuccessEvent', function(event, username) {
+		$scope.username = username;
+		$scope.password_focus = true;
+	});
 }
